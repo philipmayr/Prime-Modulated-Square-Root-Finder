58af627ba703e626f751a4c376e0da8900ec6394
Phil Samuel <philipmayr@outlook.com>
2025-06-13 10:34:06 -0500

    Update program.c

--- program.c
+++ program.c
@@ -57,20 +57,20 @@ int find_multiplicative_order(int modulus, int base)
         if (exponentiate_modularly(base, order, modulus) == 1) return order;
 }
 
-int find_modular_square_root(int square, int prime_modulus)
+int find_modular_square_root(int perfect_square, int prime_modulus)
 {
-    if (find_greatest_common_divisor(square, prime_modulus) != 1) return -1;
+    if (find_greatest_common_divisor(perfect_square, prime_modulus) != 1) return -1;
     
     int prime_modulus_less_one = prime_modulus - 1;
     
-    if (exponentiate_modularly(square, prime_modulus_less_one >> 1, prime_modulus) == prime_modulus_less_one) return -1;
+    if (exponentiate_modularly(perfect_square, prime_modulus_less_one >> 1, prime_modulus) == prime_modulus_less_one) return -1;
     
     // TODO
 }
 
 int main()
 {
-    int square = 25;
+    int perfect_square = 25;
     int prime_modulus = 37;
     
     int find_modular_square_root = find_modular_square_root(square, prime_modulus);
