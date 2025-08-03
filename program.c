28f6162bcebfdbfc2648cbecb5b0deac44bf888f
Phil Samuel <philipmayr@outlook.com>
2025-08-03 16:49:28 +0200

    Update program.c

--- program.c
+++ program.c
@@ -1,29 +1,75 @@
-// Tonelli-Shanks Algorithm
+// Prime Modulated Square Root Finder
 
 #include <stdio.h>
 
-int find_greatest_common_divisor(int a, int b)
+int count_trailing_zeroes(int number)
 {
-    int common_divisor = 1;
-    
-    while ((~a & 1) && (~b & 1))
+    int trailing_zero_count = 1;
+  
+    // 0b1111111111111111, or 2^16 - 1
+    if ((number & 0x0000FFFF) == 0)
     {
-        a >>= 1;
-        b >>= 1;
-        
-        common_divisor <<= 1;
+        trailing_zero_count += 16;
+        number >>= 16;
+    }
+  
+    // 0b11111111, or 2^8 - 1
+    if ((number & 0x000000FF) == 0)
+    {
+      trailing_zero_count += 8;
+      number >>= 8;
     }
     
-    while (a)
+    // 0b1111, or 2^4 - 1
+    if ((number & 0x0000000F) == 0)
     {
-        while (~a & 1) a >>= 1;
-        while (~b & 1) b >>= 1;
-        
-        if (a >= b) a = (a - b) >> 1;
-        else b = (b - a) >> 1;
+      trailing_zero_count += 4;
+      number >>= 4;
     }
     
-    int greatest_common_divisor = common_divisor * b;
+    // 0b11, or 2^2 - 1
+    if ((number & 0x00000003) == 0)
+    {
+      trailing_zero_count += 2;
+      number >>= 2;
+    }
+  
+    return trailing_zero_count - (number & 1);
+}
+
+int find_lesser_of_twain(int first_number, int second_number)
+{
+    return (first_number < second_number) ? first_number : second_number;
+}
+
+int get_distance_from_zero(int number)
+{
+    return (number < 0) ? -number : number;
+}
+
+int find_greatest_common_divisor(int first_number, int second_number)
+{
+    if (first_number == 0) return second_number;
+    if (second_number == 0) return first_number;
+        
+    int trailing_zero_count_of_first_number = count_trailing_zeroes(first_number);
+    int trailing_zero_count_of_second_number = count_trailing_zeroes(second_number);
+    
+    first_number >>= trailing_zero_count_of_first_number;
+    second_number >>= trailing_zero_count_of_second_number;
+    
+    int common_binary_divisor = find_lesser_of_twain(trailing_zero_count_of_first_number, trailing_zero_count_of_second_number);
+    
+    while (first_number)
+    {
+        int difference_between_numbers = first_number - second_number;
+        
+        second_number = find_lesser_of_twain(first_number, second_number);
+        first_number = get_distance_from_zero(difference_between_numbers);
+        first_number >>= count_trailing_zeroes(first_number);
+    }
+    
+    int greatest_common_divisor = second_number << common_binary_divisor;
     
     return greatest_common_divisor;
 }
@@ -160,9 +206,9 @@ int main()
             status = scanf("%d", & input);
         }
         
-        int prime_modulus_candidate = input;
+        int prime_modulus = input;
         
-        printf("Enter a quadratic residue modulo %i: ", prime_modulus_candidate);
+        printf("Enter a quadratic residue modulo %i: ", prime_modulus);
         
         // integer input validation
         // https://jackstromberg.com/2013/02/how-to-validate-numeric-integer-input-in-c/
@@ -175,7 +221,7 @@ int main()
             
             printf("Invalid input.");
             printf("\n\n");
-            printf("Enter a quadratic residue modulo %i: ", prime_modulus_candidate);
+            printf("Enter a quadratic residue modulo %i: ", prime_modulus);
             
             status = scanf("%d", & input);
         }
@@ -184,15 +230,15 @@ int main()
         
         printf("\n");
         
-        int modular_square_root = find_prime_modulated_square_root(quadratic_residue_candidate, prime_modulus_candidate);
+        int modular_square_root = find_prime_modulated_square_root(quadratic_residue_candidate, prime_modulus);
         
         if (modular_square_root == -1)
         {
-            printf("%i is not a quadratic residue modulo %i.", quadratic_residue_candidate, prime_modulus_candidate);
+            printf("%i is not a quadratic residue modulo %i.", quadratic_residue_candidate, prime_modulus);
         }
         else
         {
-            printf("The square root of %i modulated by %i is %i.", quadratic_residue_candidate, prime_modulus_candidate, modular_square_root);
+            printf("The square roots of %i modulated by %i are %i and %i.", quadratic_residue_candidate, prime_modulus, modular_square_root, prime_modulus - modular_square_root);
         }
         
         printf("\n\n");
